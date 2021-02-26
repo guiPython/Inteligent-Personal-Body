2133d496ba0e03c3cd050b057b105864ec88b4de
Guilherme Rocha <GuiPython2001@gmail.com>
2021-02-25 22:35:24 -0300

    Update HTML

--- src/scripts/Views/usuario.ts
+++ src/scripts/Views/usuario.ts
@@ -41,11 +41,31 @@ function isWindowMaximized( browserWindow : Electron.BrowserWindow ):boolean{
     return browserWindow.isMaximized()
 }
 
+function filter() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput") as HTMLInputElement;
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabela") as HTMLElement;
+    tr = table.getElementsByTagName("tr") as HTMLCollectionOf<HTMLTableRowElement>;
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } 
+            else {
+                tr[i].style.display = "none";
+                }
+        }       
+    }
+}
 
 function getCliente(){
         let cpf = ( (event as Event).currentTarget as any ).attributes.cpf.value
         ipcRenderer.send("getCliente",{id_usuario:usuario.id,cpf:cpf})
 }
+
 
 document.addEventListener("DOMContentLoaded",()=>{
 
